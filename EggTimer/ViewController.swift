7de00036afcac54b6fde219177ea3bdbf08c35dc
Old Mac <annasibirtseva@Old-MacBook-Pro.lan>
2019-11-09 22:34:57 +0200

    countdown timer and updated label

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,32 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-//    let softTime = 5
-//    let mediumTime = 7
-//    let hardTime  = 12
+    @IBOutlet weak var doneLabel: UILabel!
+    
     let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
-
+    var seconds = 0
+    var timer = Timer()
+    var secondsInMinute = 1
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle!
-        print(eggTimes[hardness]!)
         
-//        if hardness == "Soft" {
-//            print(softTime)
-//        } else if hardness == "Medium" {
-//            print(mediumTime)
-//        } else {
-//            print(hardTime)
-//        }
-//        switch hardness {
-//        case "Soft":
-//            print(softTime)
-//        case "Medium":
-//            print(mediumTime)
-//        case "Hard":
-//            print(hardTime)
-//        default:
-//            print("Error")
-//        }
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        seconds = eggTimes[hardness]! * secondsInMinute
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer(){
+        if seconds > 0 {
+        print("\(seconds) seconds.")
+            seconds -= 1
+        }
+        if seconds == 0 {
+            doneLabel.text = "DONE"
+        }
     }
     
 }
