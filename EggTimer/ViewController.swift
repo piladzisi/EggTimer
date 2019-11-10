95a4336a2421671ea2fba88f091354ebfd0ba9f9
Old Mac <annasibirtseva@Old-MacBook-Pro.lan>
2019-11-10 15:29:17 +0200

    working timer + reset button

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,32 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var doneLabel: UILabel!
     
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
-    var seconds = 0
+    let eggTimesArray = ["Soft": 5, "Medium": 7, "Hard": 12]
+    var totalCookingTime = 0
+    var secondsPassed = 0
     var timer = Timer()
-    var secondsInMinute = 1
+    var secondsInOneMinute = 1
+    
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         timer.invalidate()
         
         let hardness = sender.currentTitle!
-        seconds = eggTimes[hardness]! * secondsInMinute
+        totalCookingTime = eggTimesArray[hardness]! * secondsInOneMinute
+        doneLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer(){
-        if seconds > 0 {
-        print("\(seconds) seconds.")
-            seconds -= 1
-        }
-        else {
+        if secondsPassed < totalCookingTime  {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalCookingTime)
+            
+        } else {
             timer.invalidate()
             doneLabel.text = "DONE!"
         }
     }
+    
+    @IBAction func resetTimer(_ sender: Any) {
+        timer.invalidate()
+        secondsPassed = 0
+        progressBar.progress = 0.0
+        doneLabel.text = "How do you like your eggs?"
+        
+    }
+    
     
 }
